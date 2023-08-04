d12e7a213b5495998320b1d2f02cd4f60a05df33
magbob <zarovsky@progforce.com>
2023-08-04 22:34:10 +0100

    Add files via upload

---
+++ hello_cargo/target/debug/deps/hello_cargo-0a3dc9f7185c146c.d
@@ -0,0 +1,5 @@
+/home/pi/projects/someproject/hello_cargo/target/debug/deps/hello_cargo-0a3dc9f7185c146c: src/main.rs
+
+/home/pi/projects/someproject/hello_cargo/target/debug/deps/hello_cargo-0a3dc9f7185c146c.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/pi/projects/someproject/hello_cargo/target/debug/hello_cargo: /home/pi/projects/someproject/hello_cargo/src/main.rs
