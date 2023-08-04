acc04f366e5fd92ed79a2bcfc60cc0265dfb44ab
magbob <zarovsky@progforce.com>
2023-08-04 22:45:10 +0100

    Add files via upload

---
+++ guessing_game/target/debug/deps/guessing_game-f3887111f78f19b7.d
@@ -0,0 +1,5 @@
+/home/pi/projects/someproject/guessing_game/target/debug/deps/guessing_game-f3887111f78f19b7: src/main.rs
+
+/home/pi/projects/someproject/guessing_game/target/debug/deps/guessing_game-f3887111f78f19b7.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/pi/projects/someproject/guessing_game/target/debug/guessing_game: /home/pi/projects/someproject/guessing_game/src/main.rs
